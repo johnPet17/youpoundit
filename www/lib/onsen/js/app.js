38489fa20f0a96372f33db5cf77d79dac6b16e09
johnPet17 <jdpetalcurin@gmail.com>
2015-06-08 13:30:51 +0800

    second commit

---
+++ www/lib/onsen/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', ['onsen']);
+    
+    app.controller('MyController', function($scope){
+      	
+      	$scope.signup =function() {
+      		alert("signup!");
+      	}
+        
+});
